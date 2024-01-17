87d6145fb18e63cc66aa7f161215314b8611e11c
Andy Alberts <andyalberts@gmail.com>
2024-01-17 09:09:11 -0500

    added andy url

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return HttpResponse("Hello!")
+
+def andy(request):
+    return HttpResponse("Hello, Andy!")
