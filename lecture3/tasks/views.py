5dbb3d0e9d100a06192b41abc2e000983f2838bf
Andy Alberts <andyalberts@gmail.com>
2024-01-18 11:05:31 -0500

    created session based task list storage

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -2,15 +2,17 @@ from django import forms
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-tasks= []
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New Task")
 
 # Create your views here.
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 def add(request):
