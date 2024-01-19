77506ac242be81cd8776fe3c13e2d6bafc9f8559
Andy Alberts <andyalberts@gmail.com>
2024-01-19 12:02:30 -0500

    functions not working ?

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from .views import entry_page
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("wiki/<str:title>/", entry_page, name=entry_page)
 ]
