8cc4631346ff9c9f6336bf4561e86e0e7c2443bd
Andy Alberts <andyalberts@gmail.com>
2024-01-20 09:10:44 -0500

    Added HttpResponse to views

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from . import util
 from .util import get_entry
 
@@ -11,4 +11,7 @@ def index(request):
 
 def entry_page(request, title):
     entry_content = get_entry(title)
-    render(request, "encyclopedia/wiki.html", {"title": title, "entry_content": entry_content})
+    if entry_content:
+        return render(request, "encyclopedia/wiki.html", {"title": title, "entry_content": entry_content})
+    else:
+        return HttpResponse("Entry Not Found", status=404)
