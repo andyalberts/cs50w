caba83bdc706616b7b7509250db3bccef1e2fd34
Andy Alberts <andyalberts@gmail.com>
2024-01-20 09:19:14 -0500

    working on proper markdown translation

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . import util
 from .util import get_entry
-
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -12,6 +12,8 @@ def index(request):
 def entry_page(request, title):
     entry_content = get_entry(title)
     if entry_content:
-        return render(request, "encyclopedia/wiki.html", {"title": title, "entry_content": entry_content})
+        # Convert Markdown to HTML using markdown2
+        html_content = markdown2.markdown(entry_content)
+        return render(request, "encyclopedia/wiki.html", {"title": title, "entry_content": html_content})
     else:
         return HttpResponse("Entry Not Found", status=404)
