c725c91e3cd8c57d8c90948737bf54cb415d26bf
Andy Alberts <andyalbertss@gmail.com>
2024-01-29 08:55:50 -0500

    views cleaned up, added "non_passenger" context to flight function

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -13,7 +13,9 @@ def flight(request, flight_id):
     flight = Flight.objects.get(id=flight_id)
     return render(request, "flights/flight.html", {
         "flight": flight,
-        "passengers": flight.passengers.all()
+        "passengers": flight.passengers.all(),
+        #non_passengers Get all passengers except the ones on the flight
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
     
 def book(request, flight_id):
