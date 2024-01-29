985644cbba007a0ff6b2963d092f0847002a9f81
Andy Alberts <andyalbertss@gmail.com>
2024-01-29 10:15:31 -0500

    created html template and login

--- airline/users/views.py
+++ airline/users/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
+
+def index(request):
+    #if user not authenticated (logged in) redirect to login page
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    
+def login_request(request):
+    return render(request, "users/login.html")
