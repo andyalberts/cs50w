2cb816b499cdb415d2dca58699f69fbb9d196537
Andy Alberts <andyalbertss@gmail.com>
2024-01-29 10:50:41 -0500

    everything works to log user in and out

--- airline/users/views.py
+++ airline/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -8,9 +9,22 @@ def index(request):
     #if user not authenticated (logged in) redirect to login page
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
+    return render(request, "users/user.html")
     
 def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:  # if authentication was successful
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html", {"message":"Invalid Credentials"})
     return render(request, "users/login.html")
 
 def logout_view(request):
-    pass
+    logout(request)
+    return render(request, "users/login.html", {
+        "message": "Logged out."
+    })
