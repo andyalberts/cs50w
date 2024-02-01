148a600d088aac6a3d451f34bdc7e450c5dd932d
Andy Alberts <andyalbertss@gmail.com>
2024-02-01 10:26:19 -0500

    changed Model order

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,19 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
-class User(AbstractUser):
-    pass
-
 class Listing(models.Model):
     title = models.CharField(max_length=125)
     start_bid = models.DecimalField(max_digits=7, decimal_places=2)
     listing_image = models.ImageField(upload_to='auctions/listing_images', null=True, blank=True)
 
+class User(AbstractUser):
+    wishlist = models.ForeignKey(Listing, null=True, blank=True, on_delete=models.SET_NULL, related_name="wish_list")
 
 class Bid(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bids')
-    current_bid = models.DecimalField(max_digits=7, decimal_places=2)
+    bid_amount = models.DecimalField(max_digits=7, decimal_places=2)
 
 class Comments(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='comments')
