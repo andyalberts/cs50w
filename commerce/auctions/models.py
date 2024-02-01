2056967289e892ad5c694d20ee1158c3de8c7849
Andy Alberts <andyalbertss@gmail.com>
2024-02-01 10:15:12 -0500

    syntax correction

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -18,5 +18,5 @@ class Bid(models.Model):
 class Comments(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='comments')
     text = models.CharField(max_length=200)
-    user = models.ForeignKey(User, on_delete-models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
