c49e1335a943f67783ecc20c09a1ac20d0784cfa
Andy Alberts <andyalbertss@gmail.com>
2024-02-01 09:59:28 -0500

    Bid and Listing modified

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -8,11 +8,11 @@ class User(AbstractUser):
 class Listing(models.Model):
     title = models.CharField(max_length=125)
     start_bid = models.DecimalField(max_digits=7, decimal_places=2)
-    listing_image = models.ImageField(upload_to='static/auctions/listing_images', null=True, blank=True)
-    active 
+    listing_image = models.ImageField(upload_to='auctions/listing_images', null=True, blank=True)
 
-class Bids(models.Model):
-    start_bid = models.DecimalField(max_digits=7, decimal_places=2)
+
+class Bid(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bids')
     current_bid = models.DecimalField(max_digits=7, decimal_places=2)
 
 class Comments(models.Model):
