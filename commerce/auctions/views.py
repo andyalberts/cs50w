039802b7c15dfda8b74a8585b09cc284d52629d4
Andy Alberts <andyalbertss@gmail.com>
2024-02-02 08:33:07 -0500

    updated register, removed User.save()

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -40,10 +40,10 @@ def register(request):
     if request.method == "POST":
         username = request.POST["username"]
         email = request.POST["email"]
-
-        # Ensure password matches confirmation
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
+
+        # Ensure password matches confirmation
         if password != confirmation:
             return render(request, "auctions/register.html", {
                 "message": "Passwords must match."
@@ -52,13 +52,12 @@ def register(request):
         # Attempt to create new user
         try:
             user = User.objects.create_user(username, email, password)
-            user.save()
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
         except IntegrityError:
             return render(request, "auctions/register.html", {
                 "message": "Username already taken."
             })
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
 
