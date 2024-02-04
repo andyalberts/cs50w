130ca9c6ee62c896ee47f4473b71d64309ca1732
Andy Alberts <andyalbertss@gmail.com>
2024-02-04 13:26:33 -0500

    user admin panel created

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Listing
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    user_display=("id","listings")
+
+admin.site.register(User, UserAdmin)
