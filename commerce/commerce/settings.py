1b0ae1c6a0410e69361322aeb64e9ba82b9f4554
Andy Alberts <andyalbertss@gmail.com>
2024-02-04 12:40:51 -0500

    images appear from multiple places

--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -119,9 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static_root/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_root')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
+# C:/Users/andya/Desktop/cs50w/cs50w/commerce/static_root
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
