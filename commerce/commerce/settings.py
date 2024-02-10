73784e749720c082304e9f0be7046d28aa26f842
Andy Alberts <andyalbertss@gmail.com>
2024-02-10 13:18:11 -0500

    Changed static, stylesheets connect

--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static_root/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_root')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'C:/Users/andya/Desktop/cs50w/cs50w/commerce')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # C:/Users/andya/Desktop/cs50w/cs50w/commerce/static_root
 MEDIA_URL = '/media/'
