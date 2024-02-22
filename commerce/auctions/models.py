bbdac4e7f6eac68ba91977d529aa000adf12c156
Andy Alberts <andyalbertss@gmail.com>
2024-02-22 11:11:43 -0500

    index able to display "no bids yet" if no bids exist

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -22,7 +22,7 @@ class User(AbstractUser):
 
 class Bid(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='bids')
-    current_bid = models.DecimalField(max_digits=7, decimal_places=2)
+    current_bid = models.DecimalField(max_digits=7, decimal_places=2, default=0)
     user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE, related_name='bidder')
     created = models.DateTimeField(auto_now_add=True)
 
