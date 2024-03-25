97f1db3819edd8dc4f02f7b4f3b4198a408ed2eb
Andy Alberts <andyalbertss@gmail.com>
2024-03-25 10:44:35 -0400

    added static media path

--- network/project4/settings.py
+++ network/project4/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'network/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'network/static/')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
