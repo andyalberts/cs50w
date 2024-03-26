c0c97ac63c5feb72b50d77d9f317531ea91b1b43
Andy Alberts <andyalbertss@gmail.com>
2024-03-26 10:34:08 -0400

    added Post model

--- network/network/models.py
+++ network/network/models.py
@@ -4,3 +4,8 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0, max_digits=3)
