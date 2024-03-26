8f9bd8931b53d5fc998c06f4c5ca7023912521d1
Andy Alberts <andyalbertss@gmail.com>
2024-03-26 10:36:48 -0400

    fixed again

--- network/network/models.py
+++ network/network/models.py
@@ -8,4 +8,4 @@ class User(AbstractUser):
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
-    likes = models.IntegerField(default=0, max_length=3)
+    likes = models.IntegerField(default=0)
