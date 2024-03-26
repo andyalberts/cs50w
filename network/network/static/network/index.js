a2315355960ab496f7724872a22bf0f7cf6d8570
Andy Alberts <andyalbertss@gmail.com>
2024-03-26 11:33:30 -0400

    started infrastructure for submit-post in js

--- network/network/static/network/index.js
+++ network/network/static/network/index.js
@@ -1,6 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
-
-    document.querySelector('#post').addEventListener('click', hell);
+// button listeners 
 });
 
 
+function submit_post(event){
+    
+    // usr and datetime taken care of elsewhere
+    let text = document.getElementById('post-text').value;
+
+        fetch('/???', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                text: text
+            })
+        })
+        .then(response => response.json())
+        .then(result => {
+            console.log(result)
+        })
+}
