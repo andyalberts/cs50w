ef98c6f26c9b77410f2e9439e8077169411df27e
Andy Alberts <andyalbertss@gmail.com>
2024-04-02 11:12:38 -0400

    submit button connected to page -> create bridge for json

--- network/network/static/network/index.js
+++ network/network/static/network/index.js
@@ -1,9 +1,15 @@
+// ------------- test button ------------
+window.onload=function(){
+  var btn = document.getElementById("go-button");
+  btn.addEventListener("click", buttonClicked, true);
+
+  var submit_post = document.getElementById("submit_post");
+  submit_post.addEventListener("click", buttonClicked, true);
+}
+
 function buttonClicked(){
     document.querySelector('#go-text').innerHTML = "Go"
     console.log("Button clicked");
 }
+//----------------------------------------
 
-window.onload=function(){
-  var btn = document.getElementById("go-button");
-  btn.addEventListener("click", buttonClicked, true);
-}
