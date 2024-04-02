8eed37a551522361b667e71e2d5572805ccd1c92
Andy Alberts <andyalbertss@gmail.com>
2024-04-02 11:23:00 -0400

    QoL comments and pseudocode for submit_post func

--- network/network/static/network/index.js
+++ network/network/static/network/index.js
@@ -1,15 +1,22 @@
-// ------------- test button ------------
 window.onload=function(){
-  var btn = document.getElementById("go-button");
-  btn.addEventListener("click", buttonClicked, true);
-
-  var submit_post = document.getElementById("submit_post");
-  submit_post.addEventListener("click", buttonClicked, true);
+    var btn = document.getElementById("go-button"); //test
+    btn.addEventListener("click", buttonClicked, true); //test
+    
+    var submit_post = document.getElementById("submit_post");
+    submit_post.addEventListener("click", buttonClicked, true);
 }
 
+// ------------- test button ------------
 function buttonClicked(){
     document.querySelector('#go-text').innerHTML = "Go"
     console.log("Button clicked");
 }
 //----------------------------------------
 
+function submit_post(event){
+    // POST request to submit_post view
+    // stringify
+    // convert post to json
+    //save post, reload page to reflect new post(dynamically)
+    
+}
