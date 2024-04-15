ee5fb055a32cc3c20d7ff76981ad243cdff4a2a8
Andy Alberts <andyalbertss@gmail.com>
2024-04-15 10:48:01 -0400

    added admin panel contents

--- network/network/admin.py
+++ network/network/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User, Post
 
 # Register your models here.
+class PostInline(admin.TabularInline):  # or admin.StackedInline if you prefer
+    model = Post
+    extra = 0 
+
+class UserAdmin(admin.ModelAdmin):
+    user_display=["id"]
+    inlines = [PostInline]
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["user","text","likes","timestamp"]
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Post, PostAdmin)
